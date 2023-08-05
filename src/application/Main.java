ac2038d507253f08276be34f7ccf138e9de7b001
lukeb <lukeb@DESKTOP-56T76EH>
2023-08-04 23:16:37 -0500

    asfsdfasdfasdfasdf

--- src/application/Main.java
+++ src/application/Main.java
@@ -65,7 +65,6 @@ public class Main extends Application {
 	     
 	    	//Create Topbar Section Vboxes
 	    	
-	    	
 	    	VBox toolBarVBoxLeft = createToolBarVBoxLeft();
 	    	VBox toolBarVBoxCenter = createToolBarVBoxCenter();
 	    	VBox toolBarVBoxRight = createToolBarVBoxRight();
